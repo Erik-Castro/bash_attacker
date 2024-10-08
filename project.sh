9750754e30c7c613fb23342ef806745c0d5355f6
Erik Castro <erik_vieiracastro@hotmail.com>
2024-10-08 20:19:06 -0300

    [IMPL]: Validação de host
    Implementado a validação de host e ip

--- project.sh
+++ project.sh
@@ -44,6 +44,13 @@
 # Este software é fornecido "como está", sem qualquer garantia expressa ou implícita.
 #
 
+# Chaves
+host_alvo="localhost"
+porta_alvo="80"
+threads_atual=1
+requisicoes=100
+tempo_ataque=35
+
 # despeja uma mensagem para a saída de erro padrão com data e hora.
 msg_erro() {
     printf "[ERRO]: %s \t %s\n" "$*" "$(date +'%d/%m/%Y %X')" >&2
@@ -52,23 +59,116 @@ msg_erro() {
 # Mostra a versão atual do software
 exibe_versao() {
     echo -n "$(basename $0)"
-    grep -w "^# Versão " $0 | tail -n1 | sed 's/^#//g' | cut -d: -f1
+    grep -E "^# Versão " $0 | tail -n1 | sed 's/^#//g' | cut -d: -f1
+    printf "Copyright \u00A9 2024 Erik Castro\nUse \"--show\" para mais informações.\n\n"
+}
+
+# Exibe a Licença de SoftWare.
+exibe_licen() {
+    echo
+    awk '/# Copyright/,/# THE SOFTWARE/' $0 | tr -d "\#" | sed "s/^# //g" | sed '$d'
+}
+
+# Exibe o histórico de atualizações
+exibe_hist() {
+    echo -e " ----------\n  Versões:\n ----------"
+    grep -w "^# Versão" $0 | tr -d "\#" | sed '1d' | column -s: -t
+}
+
+# Exibe instruções de uso
+exibe_uso() {
+    echo "Uso: $0 [opções]"
+    echo "Opções:"
+    echo "  -h, --help     Exibe esta mensagem de ajuda"
+    echo "  --change-log   Exibe o histórico de mudanças"
+    echo "  --show         Exibe a licença"
+    echo "  -v, --version   Exibe a versão"
+
+}
+
+# Faz a requisição
+requisitar() {
+    local host=$1
+    local port=$2
+    echo -ne "GET / HTTP/1.1\r\nHost: $host\r\n\r\n" | nc $host $port
+}
+
+# Valida se ip é valido (IPv4)
+validar_ip() {
+    local ip="$1"
+    # Verifica se o IP está no formato correto de IPv4
+    if [[ $ip =~ ^([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}$ ]]; then
+        return 0
+    fi
+    return 1
+}
+
+# Valida se é um host válido (domínio ou IP)
+validar_host() {
+    local host="$1"
+
+    # Expressão regular para validar um nome de domínio (inclui subdomínios e domínios de TLD com mais de 3 letras)
+    if [[ $host =~ ^(([a-zA-Z0-9](-*[a-zA-Z0-9])*)\.)+[a-zA-Z]{2,}$ ]]; then
+        return 0
+    # Verifica se é um IP válido
+    elif validar_ip "$host"; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 # valida a entrada de paramêtros
 menu_check() {
-    local params="$@" # Paramêtros fornecidos
+    local params=("$@")
 
-    for param in "${params}"; do
-        case "$param" in
+    while [[ -n $1 ]]; do
+        case "$1" in
+        -host)
+            shift
+            if validar_host "$1"; then
+                host_alvo="$1"
+                shift
+            else
+                msg_erro "Host: \"$1\" inválido!"
+                return 1
+            fi
+            ;;
+        -d | --debug) set -x ;;
+        -h | --help)
+            exibe_uso
+            return 0
+            ;;
+        --change-log)
+            exibe_hist
+            return 0
+            ;;
+        --show)
+            exibe_licen
+            return 0
+            ;;
         -v | --version)
             exibe_versao
+            return 0
             ;;
         *)
-            msg_erro "Paramêtro fornecido: \"$param\", invalido!"
+            msg_erro "Paramêtro fornecido: \"$1\", invalido!"
+            return 1
             ;;
         esac
+        shift
     done
+    return 0
 }
 
-menu_check "$@"
+main() {
+    menu_check $@ || exit 1
+
+    [[ $debug_flag -eq 1 ]] && {
+        set -x
+    }
+
+    return 0
+}
+
+main $@ && echo encerrado
